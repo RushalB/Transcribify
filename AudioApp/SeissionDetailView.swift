9b72f64e4dfea92b8372bf598b5dea77c1134bed
RushalB <54552057+RushalB@users.noreply.github.com>
2025-07-07 06:01:15 -0400

    UI cleaned up

---
+++ AudioApp/SeissionDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  SeissionDetailView.swift
+//  AudioApp
+//
+//  Created by Rushal Butala on 7/7/25.
+//
+
+import SwiftUI
+
+struct SeissionDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SeissionDetailView()
+}
